99672b7941201086369b1123262e3c9e219a7d56
Victoria Kruczek <victoria.kruczek@gmail.com>
2024-08-22 13:14:19 +0200

    A user can create a new list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,4 +6,20 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.create!(set_list)
+
+    redirect_to lists_path
+  end
+
+  private
+
+  def set_list
+    params.require(:list).permit(:name)
+  end
 end
