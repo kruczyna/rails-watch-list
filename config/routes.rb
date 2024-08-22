90713fa346870eeb99891b39be2faf4290bc2936
Victoria Kruczek <victoria.kruczek@gmail.com>
2024-08-22 13:05:17 +0200

    A user can see the details of a given list and its name

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :lists, only: [:index]
+  resources :lists, only: [:index, :show]
 end
